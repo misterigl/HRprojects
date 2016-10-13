5ca777305d94e1c3d702ce573388edfac381a290
Michael Ilg <michael@ilg.ac>
2016-10-13 13:01:48 -0700

    solved _last and _each

--- src/underbar.js
+++ src/underbar.js
@@ -42,6 +42,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else {
+      return n > 0 ? array.slice(-n) : array[array.length - 1];
+    };
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -50,6 +55,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (collection.constructor === Array) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value

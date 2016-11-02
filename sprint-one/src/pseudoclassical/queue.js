11d98ee2e8da785dda01fb03375fd6f59e59b546
misterigl <michael@ilg.ac>
2016-11-02 15:35:52 -0700

    Implement size for queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,12 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.current = 0;
+
 };
 
+
+Queue.prototype.size = function() {
+  return this.current;
+};
 

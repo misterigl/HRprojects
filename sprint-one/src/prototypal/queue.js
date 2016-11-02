43aaf5b6270b4f20a712f9558f4ac5208d8e92ff
misterigl <michael@ilg.ac>
2016-11-02 15:17:28 -0700

    Implement size for queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,13 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.current = 0;
+  return queue;
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return this.current;
+};

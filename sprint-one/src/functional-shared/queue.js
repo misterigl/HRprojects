9f43cfcb90cd8c58e87ad3b4c459bb21dd2e02cf
misterigl <michael@ilg.ac>
2016-11-02 15:02:27 -0700

    Implement enqueue and dequeue for queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,7 +3,6 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var queue = {};
   queue.current = 0;
-  queue.prototype = queueMethods;
   
   for (var method in queueMethods) {
     queue[method] = queueMethods[method];
@@ -16,4 +15,24 @@ var queueMethods = {};
 
 queueMethods.size = function() {
   return this.current;
+};
+
+queueMethods.enqueue = function(value) {
+  this[this.current] = value;
+  this.current++;
+};
+
+queueMethods.dequeue = function() {
+  var result = this[0];
+  
+  for (var i = 0; i < this.current; i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[this.current];
+  
+  if (this.current > 0) {
+    this.current--;
+  }
+  
+  return result;
 };

fa23e4fce22f11ce355c288c0d4008e9dfd36143
misterigl <michael@ilg.ac>
2016-11-02 15:32:56 -0700

    Implement push and pop for stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.current = 0;
 };
 
+Stack.prototype.size = function() {
+  return this.current;
+};
+
+Stack.prototype.push = function(value) {
+  this[this.current] = value;
+  this.current++;
+};
+
+Stack.prototype.pop = function() {
+  var result = this[this.current - 1];
+  delete this[this.current - 1];
+  if (this.current > 0) {
+    this.current--;
+  }
+  return result;
+};
 

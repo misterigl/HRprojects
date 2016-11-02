45c0e54cb6878af172955a4f9eb7b8648efcb52b
misterigl <michael@ilg.ac>
2016-11-02 15:08:08 -0700

    Implement size for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.current = 0;
+
+  return stack;
 };
 
 var stackMethods = {};
 
-
+stackMethods.size = function() {
+  return this.current;
+};

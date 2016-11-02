fe20e6ede603ae238225778d24b9dd61cda5c0cb
misterigl <michael@ilg.ac>
2016-11-02 15:42:03 -0700

    Implement enqueue and dequeue for queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -10,3 +10,19 @@ Queue.prototype.size = function() {
   return this.current;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.current] = value;
+  this.current++;
+};
+
+Queue.prototype.dequeue = function() {
+  var result = this[0];
+  for (var i = 0; i < this.current; i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[this.current - 1];
+  if (this.current > 0 ) {
+    this.current--;
+  }
+  return result;
+};

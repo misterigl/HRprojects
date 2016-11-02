ac9459867f90b54345967099a70e52a006156d69
Zack Winchell <zackwinchell@gmail.com>
2016-11-02 11:54:45 -0700

    Implement stack using the functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,26 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var _storage = {};
+  var _size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    _size++;
+    _storage[_size] = value;
+    return _size;
   };
 
   someInstance.pop = function() {
+    var result = _storage[_size];
+    delete _storage[_size];
+    if (_size > 0) {
+      _size--;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return _size;
   };
 
   return someInstance;

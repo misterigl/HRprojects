b17efb6384c2b9ae140a72a9b9a0101fb2b79560
misterigl <michael@ilg.ac>
2016-11-02 20:50:29 -0700

    Implement removeEdge & add removing edges to removeNode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -20,12 +20,21 @@ Graph.prototype.contains = function(node) {
 Graph.prototype.removeNode = function(node) {
   var nodeIndex = this.nodes.indexOf(node);
   this.nodes.splice(nodeIndex, 1);
+  var edges = this.edges;
+  edges.forEach(function(edge, index) {
+    if (edge.indexOf(node) !== -1) {
+      edges.splice(index, 1);
+    }
+  });
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   return this.edges.reduce(function(hasEdge, edge) {
     if (edge[0] === fromNode && edge[1] === toNode) {
+      return true;
+    }
+    if (edge[1] === fromNode && edge[0] === toNode) {
       return true;
     }
     return false;
@@ -40,6 +49,15 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var edges = this.edges;
+  edges.forEach(function(edge, index) {
+    if (edge[0] === fromNode && edge[1] === toNode) {
+      edges.splice(index, 1);
+    }
+    if (edge[1] === fromNode && edge[0] === toNode) {
+      edges.splice(index, 1);
+    }
+  });
 };
 
 // Pass in a callback which will be executed on each node of the graph.

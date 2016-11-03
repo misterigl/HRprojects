61aeb13a435d40b032e84b834ead8eb0f609a06e
misterigl <michael@ilg.ac>
2016-11-03 10:58:24 -0700

    Add & implement findKey

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,13 +11,12 @@ HashTable.prototype.insert = function(k, v) {
     this._storage[index] = [[k, v]];
   } else if (this.hashExists) {
     let matches = this._storage[index];
-    for (var i = 0; i < matches.length; i++) { //Same hash, same key
-      if (matches[i][0] === k) {
-        matches[i][1] = v;
-        return;
-      }
+    let keyMatch = this.findKey(k);
+    if (keyMatch === -1) {
+      matches.push([k, v]); //Same hash, different key
+    } else {
+      matches[keyMatch][1] = v; //Same hash, same key
     }
-    matches.push([k, v]); //Same hash, different key
   }
 };
 
@@ -27,24 +26,34 @@ HashTable.prototype.retrieve = function(k) {
   if (!matches) {
     return undefined;
   }
-  for (var i = 0; i < matches.length; i++) {
-    if (matches[i][0] === k) {
-      return matches[i][1];
-    }
-  }
+  var keyMatch = this.findKey(k);
+  return keyMatch === -1 ? undefined : matches[keyMatch][1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var result = this._storage[index];
-  delete this._storage[index];
-  return result;
+  var matches = this._storage[index];
+  var keyMatch = this.findKey(k);
+  if (keyMatch !== -1) {
+    matches.splice(keyMatch, 1);
+  }
 };
 
 HashTable.prototype.hashExists = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var matches = this._storage[index];
   return !!matches.length;
+};
+
+HashTable.prototype.findKey = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var matches = this._storage[index];
+  for (var i = 0; i < matches.length; i++) {
+    if (matches[i][0] === k) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 /*

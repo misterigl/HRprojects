1e668fc0cd7dff6a3790e3d039b665a8c664bb37
misterigl <michael@ilg.ac>
2016-11-03 11:27:38 -0700

    Implement insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,42 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.left = null;
+  tree.right = null;
+  tree.value = value;
+
+  tree.insert = function(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value === this.value) {
+      throw new Error('Value already in tree');
+    } else {
+      if (this.right === null) {
+        this.right = BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  };
+
+  tree.contains = function() {
+
+  };
+
+  tree.depthFirstLog = function(cb) {
+    cb(this);
+    if (this.left !== null) {
+      this.left.depthFirstLog(cb);
+    }
+    if (this.right !== null) {
+      this.right.depthFirstLog(cb);
+    }
+  };
+
+  return tree;
 };
 
 

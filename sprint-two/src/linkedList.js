b7ac9b37f902da4cded74dd43685f3af672aa656
misterigl <michael@ilg.ac>
2016-11-02 19:09:50 -0700

    Fix addToTail to use references & implement removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,19 +2,25 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  list.length = 0;
 
   list.addToTail = function(value) {
     var node = Node(value);
-    this[this.length] = node;
-    this.tail = this[this.length];
-    if (this.head === null) {
-      this.head = this[this.length];
+
+    if (this.tail) {
+      this.tail.next = node;
     }
-    this.length++;
+
+    this.tail = node;
+
+    if (this.head === null) {
+      this.head = node;
+    }
   };
 
   list.removeHead = function() {
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target) {

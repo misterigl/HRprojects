7ca20de50a49611b94ee8fa06712ab2f1f59e8e7
misterigl <michael@ilg.ac>
2016-11-04 16:09:19 -0700

    Implement parents for trees & add removeFromParent

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,6 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
+  newTree.value = value || null;
+  newTree.parent = null;
 
   for (var method in treeMethods) {
     newTree[method] = treeMethods[method];
@@ -15,6 +16,7 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   var node = Tree(value);
+  node.parent = this;
   this.children.push(node);
 };
 
@@ -36,6 +38,18 @@ treeMethods.getSize = function() {
     count += this.children[i].getSize();
   }
   return count;
+};
+
+treeMethods.removeFromParent = function() {
+  var siblings = this.parent.children;
+  for (var i = 0; i < siblings.length; i++) {
+    if (_.isEqual(siblings[i], this)) {
+      siblings.splice(i, 1);
+      break;
+    }
+  }
+
+  this.parent = null;
 };
 
 /*

4f44e69efcdddcfcf9bfae232af8b4a19238c633
misterigl <michael@ilg.ac>
2016-11-05 12:28:00 -0700

    Implement checkType

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -4,10 +4,12 @@ var Set = function() {
   return set;
 };
 
+
 var setPrototype = {};
 
 setPrototype.add = function() {
   var storage = this._storage;
+
   Array.prototype.forEach.call(arguments, function(item) {
     storage[item] = true;
   });
@@ -23,6 +25,19 @@ setPrototype.remove = function(item) {
 
 setPrototype.getSize = function() {
   return Object.keys(this._storage).length;
+};
+
+var checkType = function(value) {
+  if (Array.isArray(value)) {
+    return 'array';
+  }
+  if (value === null) {
+    return 'null';
+  }
+  if (typeof value === 'object') {
+    return value.constructor.toString();
+  }
+  return typeof value;
 };
 
 /*

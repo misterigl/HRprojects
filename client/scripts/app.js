4f89eb2327729353204a076cb437023a1be8a3ab
misterigl <michael@ilg.ac>
2016-11-11 19:55:36 -0800

    Implement populating messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,30 @@
 class App {
   constructor() {
-    server: 'https://api.parse.com/1/classes/messages';
+    this.server = 'https://api.parse.com/1/classes/messages';
+    this.friends = [];
+    this.currentRoom = 'lobby';
+    this.roomList = { 'lobby': 0 };
   }
 
   init() {
+    $( '.username' ).on( 'click', function() {
+      app.handleUsernameClick($( this ).text());
+    });
 
+    $( '#messageForm' ).submit(function( event ) {
+      app.handleSubmit($('input:text').val());
+      event.preventDefault();
+    });
   }
 
   send(message) {
     $.ajax({
-      url: app.server,
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Message sent', data);
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
@@ -25,11 +35,15 @@ class App {
   fetch() {
     $.ajax({
       type: 'GET',
-      url: app.server,
+      url: this.server,
       contentType: 'application/json',
+      data: {'order': '-createdAt'},
       success: function (data) {
+        app.clearMessages();
         console.log('chatterbox: Messages received');
-        renderMessage(data.results[0]);
+        for (let i = data.results.length - 1; i >= 0; i--) {
+          app.renderMessage(data.results[i]);
+        }
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
@@ -39,8 +53,9 @@ class App {
 
   renderMessage(messageObj) {
     let username = `<p class='username'>${messageObj.username}</p>`;
+    let timeCreated = `<p class='time'>${messageObj.createdAt}</p>`;
     let message = `<p>${messageObj.text}</p>`;
-    let messageContainer = `<div>${username}${message}</div>`;
+    let messageContainer = `<div class="messageContainer">${username}${timeCreated}${message}</div>`;
     $('#chats').append(messageContainer);
   }
 
@@ -52,6 +67,19 @@ class App {
     $('#roomSelect').append(`<p>${roomName}</p>`);
   }
 
+  handleUsernameClick(userName) {
+    console.log(userName);
+  }
+
+  handleSubmit(message) {
+    let messageObj = {
+      username: (window.location.search).slice(10),
+      text: message,
+      roomname: app.currentRoom
+    };
+    app.send(messageObj);
+  }
 }
 
 let app = new App;
+app.init();

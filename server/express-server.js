994ce3533095fbf2e03ba6c74e396cd7eebc8808
Michael Ilg <michael@ilg.ac>
2016-11-20 12:06:05 -0800

    Set up node clusters and fix bower bower.json

--- server/express-server.js
+++ server/express-server.js
@@ -1,32 +1,51 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 
-app.use(express.static('client'));
-app.use(bodyParser.json());
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-var messages = [];
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  app.use(express.static('../client'));
+  app.use(bodyParser.json());
 
-app.get('/classes/messages', function (req, res) {
-  res.send({results: messages});
+  var messages = [];
 
-});
+  app.get('/classes/messages', function (req, res) {
+    res.send({results: messages});
 
-
-app.post('/classes/messages', function(req, res) {
-  res.send('Retrieved PUT message!');
-  var message = req.body;
-  message.createdAt = message.objectId = new Date().getTime();
-  // message.objectId = new Date().getTime();
-  message.roomname = message.roomname || 'lobby';
-  message.updatedAt = message.createdAt;
-
-  messages.unshift(message);
+  });
 
 
+  app.post('/classes/messages', function(req, res) {
+    res.send('Retrieved PUT message!');
+    var message = req.body;
+    message.createdAt = message.objectId = new Date().getTime();
+    // message.objectId = new Date().getTime();
+    message.roomname = message.roomname || 'lobby';
+    message.updatedAt = message.createdAt;
 
-});
+    messages.unshift(message);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+
+
+  });
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+}
+
+
+

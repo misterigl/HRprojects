43252f16e987848147627c7a38d0812fae0b6a61
Michael Ilg <michael@ilg.ac>
2017-01-23 15:06:22 -0800

    Let players take turns

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,9 @@
+var player1 = true
+
 function setPlayer(location) {
   var field = document.getElementById(location)
-  field.innerText = 'X';
-  console.log(this, location)
+  field.innerText = player1 ? 'X' : 'O'
+  player1 = !player1
+  var textField = document.getElementById('textField')
+  textField.innerText = player1 ? 'Player 1' : 'Player 2'
 }

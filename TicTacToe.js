d9bb0b2e031082c92e0543b28a2f5de24eb0b7bc
Michael Ilg <michael@ilg.ac>
2017-01-23 15:31:06 -0800

    check winning condition

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,5 @@
 var player1 = true
-var fieldArray = [[],[],[]]
+var fieldArray = [[0,0,0],[0,0,0],[0,0,0]]
 
 function setPlayer(location) {
   var field = document.getElementById(location)
@@ -10,35 +10,46 @@ function setPlayer(location) {
 
   switch (location) {
   case 'left-top':
-    fieldArray[0][0] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[0][0] = player1 ? -1 : 1
+    break
   case 'middle-top':
-    fieldArray[0][1] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[0][1] = player1 ? -1 : 1
+    break
   case 'right-top':
-    fieldArray[0][2] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[0][2] = player1 ? -1 : 1
+    break
   case 'left-middle':
-    fieldArray[1][0] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[1][0] = player1 ? -1 : 1
+    break
   case 'middle-middle':
-    fieldArray[1][1] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[1][1] = player1 ? -1 : 1
+    break
   case 'right-middle':
-    fieldArray[1][2] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[1][2] = player1 ? -1 : 1
+    break
   case 'left-bottom':
-    fieldArray[2][0] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[2][0] = player1 ? -1 : 1
+    break
   case 'middle-bottom':
-    fieldArray[2][1] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[2][1] = player1 ? -1 : 1
+    break
   case 'right-bottom':
-    fieldArray[2][2] = player1 ? 'X' : 'O'
-    break;
+    fieldArray[2][2] = player1 ? -1 : 1
+    break
   default:
     //Statements executed when none of the values match the value of the expression
-    break;
+    break
   }
-  console.log(fieldArray);
+  console.log(fieldArray, (fieldArray[0][0] + fieldArray[0][1] + fieldArray[0][2]))
+
+  // win conditions
+  if (fieldArray[0][0] + fieldArray[0][1] + fieldArray[0][2] === 3 ) {
+    textField.innerText = 'Player 1 won'
+  }
+  // plenty more to come, a loop probably would be better, or recursion
+  // for (var i = 0; i < fieldArray.length; i++) {
+  //   for (var j = 0; j < fieldArray.length; j++) {
+  //     fieldArray[i]
+  //   }
+  // }
 }
